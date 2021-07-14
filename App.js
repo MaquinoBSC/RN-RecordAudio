861853c35f775b2eefe92b406b9fb60f8ee1d18d
MaquinoBSC <martinare.ipn@gmail.com>
2021-07-14 16:04:57 -0500

    Creacion de la app

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import AudioRecorderPlayer from './src/AudioRecorderPlayer';
+
+
+export default function App(){
+  return(
+    <>
+      <AudioRecorderPlayer />
+    </>
+  )
+}
---
+++ src/AudioRecorderPlayer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function AudioRecorderPlayer(){
+    return(
+        <View>
+            <Text>Audio Recorder Player</Text>
+        </View>
+    )
+}
